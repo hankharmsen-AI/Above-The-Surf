9172f4474a8f68be3c0858542749191811019e42
hankharmsen-AI <hankharmsen@gmail.com>
2026-09-02 16:57:55 -0500

    Delete Above the surf website directory

--- Above the surf website/fix_body.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('css/style.css', 'r', encoding='utf-8') as f:
-    css = f.read()
-
-css = css.replace('body {', 'body { overflow-x: hidden;')
-
-with open('css/style.css', 'w', encoding='utf-8') as f:
-    f.write(css)
